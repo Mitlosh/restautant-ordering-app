1ac79808fe7372fcf6303d521a23f45d6e6cd276
Vladyslav Mitlosh <112478986+Mitlosh@users.noreply.github.com>
2022-11-30 12:24:25 +0200

    Upload files

---
+++ data.js
@@ -0,0 +1,23 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: "0",
+        price: 14,
+        image: "/images/item-graphic-1.png"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        image: "/images/item-graphic-2.png",
+        id: "1"
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        image: "/images/item-graphic-3.png",
+        id: "2"
+    }
+]
---
+++ index.js
@@ -0,0 +1,107 @@
+import { menuArray } from './data.js'
+const orderArray = []
+
+document.addEventListener('click', (e) => {
+    if(e.target.dataset.btn) {
+        getOrderField(e.target.dataset.btn)
+    } else if(e.target.dataset.remove) {
+        removeFromOrder(e.target.dataset.remove)
+    }
+    
+})
+
+function getOrderField(foodId){
+    const targetMealObj = menuArray.filter(meal => meal.id === foodId)[0]
+    orderArray.push(targetMealObj)
+    
+    const uniqueChars = orderArray.filter((item, index) => {
+        const isdDuplicate = orderArray.indexOf(item) === index
+        if (isdDuplicate) {
+            return isdDuplicate
+        }
+        console.log(item.price + item.price)
+    })
+    // console.log(uniqueChars)
+    
+    let orderHtml = ``
+    uniqueChars.map(item => {
+        orderHtml += `
+        <div class="order-detail">
+            <h2>${item.name} <span data-remove=${item.id} id="remove">remove</span></h2>        
+            <p>$${item.price}</p>
+        </div>
+    `
+    })
+    
+    let totalOrder = `
+        <div class="container">
+            <h2>Your order</h2>
+            ${orderHtml}
+            <div class="total-price">
+                <h2>Total price:</h2>
+                <p>$14</p>
+            </div>
+            <button>Complete order</button>
+        </div>
+    `
+    document.getElementById('total-order').innerHTML = totalOrder
+}
+
+function removeFromOrder(removeId) {
+    orderArray.map(meal => {
+        if(meal.id === removeId) orderArray.splice(meal, 1)
+    })
+}
+
+function getMenuHtml(){    
+    let menuHtml = ``
+    
+    menuArray.map(meal => {        
+        menuHtml += `
+        <div class="container">
+        <div class="section">
+            <div class="food-selection">
+                <img class="food-img" src="${meal.image}" alt="pizza-img">
+                <div>
+                    <h2>${meal.name}</h2>
+                    <p>${meal.ingredients}</p>
+                    <p>$${meal.price}</p>  
+                </div>  
+            </div>
+            <button data-btn="${meal.id}">+</button>
+        </div>
+    </div>
+        `   
+    })    
+    return menuHtml
+}
+
+function getOrderHtml() {
+    let orderHtml = ``
+    
+    
+}
+
+function render(){
+    document.getElementById('foodMenu').innerHTML = getMenuHtml()
+}
+
+render()
+
+
+
+
+{/* ORDER
+<div class="container">
+    <h2>Your order</h2>
+    <div class="order-detail">
+        <h2>${targetMealObj.name} <span id="remove">remove</span></h2>        
+        <p>$${targetMealObj.price}</p>
+    </div>
+    <div class="total-price">
+        <h2>Total price:</h2>
+        <p>$14</p>
+    </div>
+    <button>Complete order</button>
+</div>
+*/}
