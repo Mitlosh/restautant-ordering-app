9a5bd49128fd5953082d657558713292c682c84b
Vladyslav <vlad.mitlosh@gmail.com>
2023-04-29 10:05:02 +0300

    deploy

--- data.js
+++ data.js
@@ -1,26 +1,26 @@
 export const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        price: 14,
-        image: "/images/item-graphic-1.png",
-        amount: 1,
-        id: "0"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        image: "/images/item-graphic-2.png",
-        amount: 1,
-        id: "1"
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain", "hops", "yeast", "water"],
-        price: 8,
-        image: "/images/item-graphic-3.png",
-        amount: 1,
-        id: "2"
-    }
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    price: 14,
+    image: "images/item-graphic-1.png",
+    amount: 1,
+    id: "0",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    image: "images/item-graphic-2.png",
+    amount: 1,
+    id: "1",
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain", "hops", "yeast", "water"],
+    price: 8,
+    image: "images/item-graphic-3.png",
+    amount: 1,
+    id: "2",
+  },
 ]
--- index.js
+++ index.js
@@ -1,57 +1,58 @@
-import { menuArray } from './data.js'
+import { menuArray } from "./data.js"
 
 const orderArray = []
 
-document.addEventListener('click', (e) => {
-    if(e.target.dataset.btn) {
-        handleAddBtn(e.target.dataset.btn)
-    } else if(e.target.dataset.remove) {
-        handleRemoveBtn(e.target.dataset.remove)
-    } 
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.btn) {
+    handleAddBtn(e.target.dataset.btn)
+  } else if (e.target.dataset.remove) {
+    handleRemoveBtn(e.target.dataset.remove)
+  }
 })
 
-function handleAddBtn(foodId){
-    const targetMealObj = menuArray.filter(meal => meal.id === foodId)[0]
-    const duplicate = orderArray.includes(targetMealObj)
+function handleAddBtn(foodId) {
+  const targetMealObj = menuArray.filter((meal) => meal.id === foodId)[0]
+  const duplicate = orderArray.includes(targetMealObj)
 
-    if(!duplicate) orderArray.push(targetMealObj)
-    else orderArray.filter(meal => {
-        return meal.id === foodId ? meal.amount ++ : meal.amount
+  if (!duplicate) orderArray.push(targetMealObj)
+  else
+    orderArray.filter((meal) => {
+      return meal.id === foodId ? meal.amount++ : meal.amount
     })
-    console.log(orderArray)
-    getOrderHtml()
+  console.log(orderArray)
+  getOrderHtml()
 }
 
 function totalCost() {
-    const totalCost = orderArray.reduce((acc, meal) => {
-        return acc + meal.price * meal.amount        
-    }, 0)
-    return totalCost
+  const totalCost = orderArray.reduce((acc, meal) => {
+    return acc + meal.price * meal.amount
+  }, 0)
+  return totalCost
 }
 
 function handleRemoveBtn(foodId) {
-    const targetObj = orderArray.filter(meal => {
-        return meal.id === foodId
-    })[0]
-    const indexToDelete = orderArray.findIndex(order => {
-        return order.id === targetObj.id
-    })
-    
-    orderArray.filter(meal => {
-        if (meal.id === foodId && meal.amount > 1) {
-            meal.amount -= 1
-        } else if (meal.amount === 1) {
-            orderArray.splice(indexToDelete, 1)
-            return orderArray
-        }
-    })
-    getOrderHtml()
+  const targetObj = orderArray.filter((meal) => {
+    return meal.id === foodId
+  })[0]
+  const indexToDelete = orderArray.findIndex((order) => {
+    return order.id === targetObj.id
+  })
+
+  orderArray.filter((meal) => {
+    if (meal.id === foodId && meal.amount > 1) {
+      meal.amount -= 1
+    } else if (meal.amount === 1) {
+      orderArray.splice(indexToDelete, 1)
+      return orderArray
+    }
+  })
+  getOrderHtml()
 }
 
 function getOrderHtml() {
-    let orderHtml = ``
-    orderArray.map(item => {
-        orderHtml += `
+  let orderHtml = ``
+  orderArray.map((item) => {
+    orderHtml += `
         <div class="order-detail">
             <h2>${item.name} ${item.amount}
                 <span data-remove=${item.id} class="remove-btn">
@@ -61,9 +62,9 @@ function getOrderHtml() {
             <p>$${item.price * item.amount}</p>
         </div>
     `
-    })
-    
-    let totalOrder = `
+  })
+
+  let totalOrder = `
         <div class="container">
             <h2>Your order</h2>
             ${orderHtml}
@@ -74,38 +75,36 @@ function getOrderHtml() {
             
         </div>
     `
-    document.querySelector('.order').innerHTML = totalOrder
+  document.querySelector(".order").innerHTML = totalOrder
 }
 
-const completeOrderBtn = document.querySelector('.complete-order-btn')
-const payForm = document.querySelector('.pay-form')
+const completeOrderBtn = document.querySelector(".complete-order-btn")
+const payForm = document.querySelector(".pay-form")
 
-completeOrderBtn.addEventListener('click', () => {
-    if (orderArray.length) payForm.classList.remove('hidden')
+completeOrderBtn.addEventListener("click", () => {
+  if (orderArray.length) payForm.classList.remove("hidden")
 })
 
-function getMenuHtml(){    
-    let menuHtml = ``
-    
-    menuArray.map(meal => {        
-        menuHtml += `
+function getMenuHtml() {
+  let menuHtml = ``
+
+  menuArray.map((meal) => {
+    menuHtml += `
         <div class="container">
         <div class="section">
             <div class="food-selection">
-                <img class="food-img" src="${meal.image}" alt="pizza-img">
                 <div>
                     <h2>${meal.name}</h2>
-                    <p>${meal.ingredients.join(', ')}</p>
+                    <p>${meal.ingredients.join(", ")}</p>
                     <p>$${meal.price}</p>  
                 </div>  
             </div>
             <button data-btn="${meal.id}">+</button>
         </div>
     </div>
-        `   
-    })    
-    document.getElementById('foodMenu').innerHTML = menuHtml
+        `
+  })
+  document.getElementById("foodMenu").innerHTML = menuHtml
 }
 
 getMenuHtml()
-
